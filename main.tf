55b2e2f3bcf81a01d24357dff15a7970c252f0d8
vvijay2468 <vvijay2468@gmail.com>
2024-10-06 16:45:35 +0530

    Task_2_done

--- main.tf
+++ main.tf
@@ -81,33 +81,6 @@ resource "azurerm_network_security_group" "allowedports" {
 }
 
 
-resource "azurerm_public_ip" "webserver_public_ip_vm1" {
-   name                = "webserver-public-ip-vm1"
-   location            = var.location
-   resource_group_name = azurerm_resource_group.Res_t.name
-   allocation_method   = "Dynamic"
-
-   tags = {
-       environment = var.environment
-       costcenter  = "it"
-   }
-
-   depends_on = [azurerm_resource_group.Res_t]
-}
-
-resource "azurerm_public_ip" "webserver_public_ip_vm2" {
-   name                = "webserver-public-ip-vm2"
-   location            = var.location
-   resource_group_name = azurerm_resource_group.Res_t.name
-   allocation_method   = "Dynamic"
-
-   tags = {
-       environment = var.environment
-       costcenter  = "it"
-   }
-
-   depends_on = [azurerm_resource_group.Res_t]
-}
 
 resource "azurerm_network_interface" "vm1" {
    name                = "nginx-interface-vm1"
@@ -118,7 +91,6 @@ resource "azurerm_network_interface" "vm1" {
        name                          = "internal"
        private_ip_address_allocation = "Dynamic"
        subnet_id                    = azurerm_subnet.subnet1.id
-       public_ip_address_id         = azurerm_public_ip.webserver_public_ip_vm1.id
    }
 
    depends_on = [azurerm_resource_group.Res_t]
@@ -133,7 +105,6 @@ resource "azurerm_network_interface" "vm2" {
        name                          = "internal"
        private_ip_address_allocation = "Dynamic"
        subnet_id                    = azurerm_subnet.subnet1.id
-       public_ip_address_id         = azurerm_public_ip.webserver_public_ip_vm2.id
    }
 
    depends_on = [azurerm_resource_group.Res_t]
@@ -168,6 +139,7 @@ resource "azurerm_linux_virtual_machine" "vm1" {
        caching              = "ReadWrite"
        storage_account_type = "Standard_LRS"
    }
+   depends_on = [ azurerm_availability_set.set1 ]
 
    tags = {
        environment = var.environment
@@ -201,6 +173,8 @@ resource "azurerm_linux_virtual_machine" "vm2" {
        caching              = "ReadWrite"
        storage_account_type = "Standard_LRS"
    }
+   depends_on = [ azurerm_availability_set.set1 ]
+
 
    tags = {
        environment = var.environment
@@ -208,3 +182,101 @@ resource "azurerm_linux_virtual_machine" "vm2" {
    }
 }
 
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.allowedports.id
+  depends_on = [
+    azurerm_network_security_group.allowedports
+  ]
+}
+
+resource "azurerm_public_ip" "load_ip" {
+  name                = "load-ip"
+  location            = azurerm_resource_group.Res_t.location
+  resource_group_name = azurerm_resource_group.Res_t.name
+  allocation_method   = "Static"
+  sku="Standard"
+}
+
+resource "azurerm_lb" "app_balancer" {
+  name                = "app-balancer"
+  location            = azurerm_resource_group.Res_t.location
+  resource_group_name = azurerm_resource_group.Res_t.name
+  sku="Standard"
+  sku_tier = "Regional"
+  frontend_ip_configuration {
+    name                 = "frontend-ip"
+    public_ip_address_id = azurerm_public_ip.load_ip.id
+  }
+
+  depends_on=[
+    azurerm_public_ip.load_ip
+  ]
+}
+
+// Here we are defining the backend pool
+resource "azurerm_lb_backend_address_pool" "PoolA" {
+  loadbalancer_id = azurerm_lb.app_balancer.id
+  name            = "PoolA"
+  depends_on=[
+    azurerm_lb.app_balancer
+  ]
+}
+
+resource "azurerm_lb_backend_address_pool_address" "appvm1_address" {
+  name                    = "appvm1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.PoolA.id
+  virtual_network_id      = azurerm_virtual_network.vnet1.id
+  ip_address              = azurerm_network_interface.vm1.private_ip_address
+  depends_on=[
+    azurerm_lb_backend_address_pool.PoolA
+  ]
+}
+
+resource "azurerm_lb_backend_address_pool_address" "appvm2_address" {
+  name                    = "appvm2"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.PoolA.id
+  virtual_network_id      = azurerm_virtual_network.vnet1.id
+  ip_address              = azurerm_network_interface.vm2.private_ip_address
+  depends_on=[
+    azurerm_lb_backend_address_pool.PoolA
+  ]
+}
+
+
+// Here we are defining the Health Probe
+resource "azurerm_lb_probe" "app_lb_probe" {
+  name                = "app-lb-http-probe"
+  loadbalancer_id     = azurerm_lb.app_balancer.id
+  protocol            = "Http"
+  port                = 80
+  request_path        = "/"
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+// Here we are defining the Load Balancing Rule
+resource "azurerm_lb_rule" "RuleA" {
+  loadbalancer_id                = azurerm_lb.app_balancer.id
+  name                           = "RuleA"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "frontend-ip"
+  backend_address_pool_ids = [ azurerm_lb_backend_address_pool.PoolA.id ]
+  depends_on=[
+    azurerm_lb.app_balancer
+  ]
+}
+
+// This is used for creating the NAT Rules
+
+resource "azurerm_lb_nat_rule" "NATRuleA" {
+  resource_group_name            = azurerm_resource_group.Res_t.name
+  loadbalancer_id                = azurerm_lb.app_balancer.id
+  name                           = "ssh"
+  protocol                       = "Tcp"
+  frontend_port                  = 22
+  backend_port                   = 22
+  frontend_ip_configuration_name = "frontend-ip"
+  depends_on                     = [azurerm_lb.app_balancer]
+}
--- output.tf
+++ output.tf
@@ -1,15 +1,50 @@
-output "vnet_subnets" {
- value = azurerm_subnet.subnet1.id
+output "resource_group_name" {
+  description = "The name of the resource group created."
+  value       = azurerm_resource_group.Res_t.name
 }
 
-output "vnet_id" {
- value = azurerm_virtual_network.vnet1.id
+output "virtual_network_id" {
+  description = "The ID of the virtual network created."
+  value       = azurerm_virtual_network.vnet1.id
 }
 
-output "vm1_public_ip" {
-   value = azurerm_public_ip.webserver_public_ip_vm1.ip_address
+output "subnet_id" {
+  description = "The ID of the subnet created."
+  value       = azurerm_subnet.subnet1.id
 }
 
-output "vm2_public_ip" {
-   value = azurerm_public_ip.webserver_public_ip_vm2.ip_address
+output "availability_set_name" {
+  description = "The name of the availability set created."
+  value       = azurerm_availability_set.set1.name
+}
+
+output "vm1_private_ip" {
+  description = "The private IP address of VM1."
+  value       = azurerm_network_interface.vm1.private_ip_address
+}
+
+output "vm2_private_ip" {
+  description = "The private IP address of VM2."
+  value       = azurerm_network_interface.vm2.private_ip_address
+}
+
+
+output "load_balancer_public_ip" {
+  description = "The public IP address of the load balancer."
+  value       = azurerm_public_ip.load_ip.ip_address
+}
+
+output "backend_pool_id" {
+  description = "The ID of the backend pool for the load balancer."
+  value       = azurerm_lb_backend_address_pool.PoolA.id
+}
+
+output "health_probe_name" {
+  description = "The name of the health probe used for the load balancer."
+  value       = azurerm_lb_probe.app_lb_probe.name
+}
+
+output "load_balancing_rule_name" {
+  description = "The name of the load balancing rule."
+  value       = azurerm_lb_rule.RuleA.name
 }
